572edc7a928650770a532d3de7fded0c73add2e8
manjunath528 <bmanjunath505@gmail.com>
2025-12-26 16:55:59 +0100

    added the logs with the traceID for services

---
+++ analytics-service/src/main/java/com/pm/analyticsservice/filter/CorrelationIdFilter.java
@@ -0,0 +1,34 @@
+package com.pm.analyticsservice.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class CorrelationIdFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws IOException, ServletException {
+
+        String correlationId = request.getHeader("X-Correlation-Id");
+
+        if (correlationId != null) {
+            MDC.put("correlationId", correlationId);
+        }
+
+        try {
+            filterChain.doFilter(request, response);
+        } finally {
+            MDC.clear();
+        }
+    }
+}
