b7400b237e93857eacd789408e934d9b41cea807
manjunath528 <bmanjunath505@gmail.com>
2026-01-02 20:25:57 +0100

    integration testing

--- integration-tests/src/test/java/UserIntegrationTest.java
+++ integration-tests/src/test/java/UserIntegrationTest.java
@@ -18,8 +18,8 @@ public class UserIntegrationTest extends BaseIntegrationTest {
     void shouldCreateUser() {
         String payload = """
                           {
-                            "name": "maaaasssss",
-                            "email": "masssssss.sharma@example.com",
+                            "name": "sasaaa",
+                            "email": "sasaaa.sharma@example.com",
                             "address": "123 Main Street, Apt 22",
                             "dateOfBirth": "2000-09-09",
                             "weight": 60.5,
